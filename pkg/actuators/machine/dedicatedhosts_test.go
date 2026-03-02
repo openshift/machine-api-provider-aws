4ec4184ade5df8e48a5fa74e2bd4db1a2a58ae81
Brandon Palm <bpalm@redhat.com>
2026-03-02 14:59:35 -0600

    Fix missed import in dedicatedhosts_test.go
    Update the remaining github.com/golang/mock import to go.uber.org/mock
    that was missed in the initial migration.

--- pkg/actuators/machine/dedicatedhosts_test.go
+++ pkg/actuators/machine/dedicatedhosts_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/golang/mock/gomock"
+	"go.uber.org/mock/gomock"
 	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
 	"github.com/openshift/machine-api-provider-aws/pkg/client/mock"
 	"k8s.io/utils/ptr"
